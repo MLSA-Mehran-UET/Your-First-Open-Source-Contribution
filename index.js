555826f47accdfde17d491614c80e8fa66bb09fe
Owais <owais.leghari@hotmail.com>
2020-10-10 11:19:42 +0500

    pushed first version

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//Serves resources from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+const fs = require('fs');
+
+
+
+app.get('/', (req, res) => {
+    let rawdata = fs.readFileSync('./Data/contributors.json');
+    let contributors = JSON.parse(rawdata);
+    console.log(contributors);
+
+    res.render('index', { members: contributors })
+})
+
+//connection to server
+
+app.listen(process.env.PORT||3000, error => {
+    if (error) console.log("error is " + error);
+    else console.log("listening to port " + 3000);
+
+});
+
+// For running this project locally.
+// app.listen(3000);
